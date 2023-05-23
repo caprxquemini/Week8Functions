f04c33c6751c64b80b7758fa2e2c3f9fa8e72afe
caprxquemini <stefaniabarabula12@gmail.com>
2023-05-23 16:11:41 +0300

    Add project files.

---
+++ FunctionsWithReturn/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] movies = { "The Sound of Music", "The Godfather", "10 Things I Hate About You", "Titanic", "Space Jam" };
+string[] drinks = { "Coca Cola", "lemonade", "milkshake", "Mountain Dew", "tea" };
+string[] food = { "fries", "salad", "chicken wrap", "lasagna", "popcorn" };
+
+string randomMovie = PickRandom(movies);
+string randomDrink = PickRandom(drinks);
+string randomFood = PickRandom(food);
+
+Console.WriteLine($"I recommend you to watch {randomMovie} tonight.");
+Console.WriteLine($"And also to drink {randomDrink} and eat {randomFood}.");
+
+static string PickRandom(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomElement = someArray[randomIndex];
+    return randomElement;
+}
---
+++ Week8Functions/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] movies = { "The Sound of Music", "The Godfather", "10 Things I Hate About You", "Titanic", "Space Jam"};
+string[] drinks = { "Coca Cola", "lemonade", "milkshake", "Mountain Dew", "tea" };
+string[] food = { "fries", "salad", "chicken wrap", "lasagna", "popcorn" };
+
+PickRandomFromArray(movies);
+PickRandomFromArray(drinks);
+PickRandomFromArray(food);
+
+static void PickRandomFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomElement = someArray[randomIndex];
+    Console.WriteLine(randomElement);
+}
